06c7ed6ef7c471dfae65530f83d875d943dcc008
Michalina Pudlo <pudlomichalina203@gmail.com>
2022-11-28 20:40:58 +0100

    first commit

---
+++ src/main/java/pl/coderslab/charity/model/Category.java
@@ -0,0 +1,12 @@
+package pl.coderslab.charity.model;
+
+//@Entity
+//@Data
+//@Table(name="category")
+//public class Category {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//
+//}
